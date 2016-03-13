746d01478bdd9ad88c71176156ec5d055e9f4bad
tao_k <tao.keomony@gmail.com>
2016-03-13 14:44:15 +0100

    ajout de l'inscription

--- app/www/scripts/controllers/SignCtrl.js
+++ app/www/scripts/controllers/SignCtrl.js
@@ -1,5 +1,7 @@
-app.controller('SignCtrl', function($scope, $stateParams) {
-
+app.controller('SignCtrl', function($scope, $stateParams, $location) {
+	$scope.go = function() {
+		$location.path('/app/sign/1');
+	}
 });
 
 app.controller('Sign1Ctrl', function($scope, $stateParams) {
