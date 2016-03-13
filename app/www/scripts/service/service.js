811fd1592ef55897ee5e276457ff4c81341a22c1
ENAUX Guillaume <guillaume.enaux@epitech.eu>
2016-03-13 12:28:35 +0100

    avancement dans le fichier de traduction

---
+++ app/www/scripts/service/service.js
@@ -0,0 +1,26 @@
+app.service('Translate', function() {
+  this.get = function() {
+    return {
+      "france": {
+        "news": "Actualitées",
+        "home": "Accueil",
+        "viewService" : "Voir les services",
+        "viewRequest" : "Voir les demandes",
+        "notification" : "Notification",
+        "profil": "Profil",
+        "message" : "Message",
+        "setting": "Paramétres",
+        list: {  title: "List" , titlePage: "Employée", map: "Carte", link:  "Listes", filters : "Filtres", textFormInputSearch: "Rechercher" }
+
+      },
+      "arabe": {
+        "news": "Blababla",
+        "home": "le titre de la page accueil",
+        "viewService" : "deuxieme lien dans le menu",
+        "viewRequest" : "troisieme lien dans le menu",
+        "notification": "notification"
+
+      }
+    }
+  }
+});
